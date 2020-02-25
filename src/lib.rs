9f834d5fa5372f6dfa7430029d2a297ea02e72f0
shuaixr <shuaixr@163.com>
2020-02-25 18:03:09 +0800

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     let results = if config.case_sensitive {
-        search(&config.query,&contents)
-    }else{
+        search(&config.query, &contents)
+    } else {
         search_case_insensitive(&config.query, &contents)
     };
     for line in results {
@@ -17,7 +17,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
-    for line in contents.lines(){
+    for line in contents.lines() {
         if line.to_lowercase().contains(&query) {
             results.push(line)
         }
@@ -46,7 +46,11 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-        Ok(Config { query, filename, case_sensitive})
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 #[cfg(test)]
@@ -70,7 +74,9 @@ Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-        assert_eq!(vec!["Rust:","Trust me."],search_case_insensitive(query,contents))
-
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
     }
 }
